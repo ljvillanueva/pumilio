f026b0b9e73ccfea9bbcb2c97e59b68a77e51bc7
ljvillanueva <ljvillanueva@coquipr.com>
2013-04-16 18:17:43 -0400

    Adding some form checks

--- www/add_from_db.php
+++ www/add_from_db.php
@@ -87,63 +87,60 @@ if ($use_googleanalytics){
 			<?php
 			echo "<h3>Add files from a database or spreadsheet</h3>
 			<form action=\"add_from_db2.php\" method=\"POST\" id=\"AddForm\">
-				<p>First, upload the files to a directory in the server that the webserver can read.<br>";
-			
-				$temp_add_dir=query_one("SELECT Value from PumilioSettings WHERE Settings='temp_add_dir'", $connection);
-				if ($temp_add_dir!=""){
-					if (substr($temp_add_dir, -1, 1)!="/"){
-						$temp_add_dir = $temp_add_dir . "/";
-						}
-					echo "($temp_add_dir):<br>
-						Directory: <select name=\"dir\" class=\"ui-state-default ui-corner-all\">\n";
-						#From http://stackoverflow.com/questions/2428795/php-ordered-directory-listing
-						$dir = glob($temp_add_dir . "*", GLOB_ONLYDIR);
-						sort($dir,SORT_STRING);
-						foreach($dir as $this_dir) { 
-							echo "<option>$this_dir</option>\n";
-							} 
-							
-						echo "</select>";
+			<p>First, upload the files to a directory in the server that the webserver can read.<br>";
+		
+			$temp_add_dir=query_one("SELECT Value from PumilioSettings WHERE Settings='temp_add_dir'", $connection);
+			if ($temp_add_dir == ""){
+				echo "<div class=\"error\">The local directory is not set. Please set it up in the <a href=\"admin.php\">Administration</a> menu.</div>";
+				$valid_form = 0;
+				}
+			else{
+				$localdir = $temp_add_dir;
+				echo "<p>Server local directory: <input name=\"localdir\" type=\"text\" maxlength=\"160\" size=\"50\" value=\"$localdir\" class=\"fg-button ui-state-default ui-corner-all\">";
+				$valid_form = 1;
+				}
+				
+			echo "<input type=\"hidden\" name=\"local\" value=\"1\">";
+				
+			echo "<br>Select the format of the files: 
+				<select name=\"files_format\" class=\"ui-state-default ui-corner-all\">
+				<option></option>";
+				require("include/sox_formats_list.php");
+				//SoX options
+				for ($s=0;$s<count($sox_formats);$s++) {
+					echo "<option>$sox_formats[$s]</option>";
 					}
-				else {
-					echo "(not set - please tell admin to set this value)<br>
-					Directory: <select name=\"dir\" class=\"ui-state-default ui-corner-all\">
-					</select>";
-					}
-			
-				echo "<br>Select the format of the files: 
-					<select name=\"files_format\" class=\"ui-state-default ui-corner-all\">
-						<option></option>";
-						require("include/sox_formats_list.php");
-						//SoX options
-						for ($s=0;$s<count($sox_formats);$s++) {
-							echo "<option>$sox_formats[$s]</option>";
-							}
-							
-					echo "</select><br>";
+						
+			echo "</select><br>";
 
-				$query = "SELECT * from Collections ORDER BY CollectionName";
-				$result = mysqli_query($connection, $query)
-					or die (mysqli_error($connection));
-				$nrows = mysqli_num_rows($result);
+			$query = "SELECT * from Collections ORDER BY CollectionName";
+			$result = mysqli_query($connection, $query)
+				or die (mysqli_error($connection));
+			$nrows = mysqli_num_rows($result);
 
-				if ($nrows>0) {
-					echo "Add files to this source: 
-					<select name=\"ColID\" class=\"ui-state-default ui-corner-all\">
-						<option></option>";
-						for ($i=0;$i<$nrows;$i++) {
-							$row = mysqli_fetch_array($result);
-							extract($row);
-							echo "<option value=\"$ColID\">$CollectionName</option>\n";
-							}
-					echo "</select> <a href=\"add_source.php\">Add Collections</a><br>
-					<input type=submit value=\" Check \" class=\"fg-button ui-state-default ui-corner-all\">";
+			if ($nrows>0) {
+				echo "Add files to this source: 
+				<select name=\"ColID\" class=\"ui-state-default ui-corner-all\">
+					<option></option>";
+				for ($i=0;$i<$nrows;$i++) {
+					$row = mysqli_fetch_array($result);
+					extract($row);
+					echo "<option value=\"$ColID\">$CollectionName</option>\n";
 					}
-				else {
-					echo "<p><strong>There are no collections in the archive. Please create at least one to continue.</strong>";
+				echo "</select> <a href=\"add_source.php\">Add Collections</a><br>";
+				
+				if ($valid_form == 0){
+					echo "<input type=submit value=\" Check \" DISABLED class=\"fg-button ui-state-default ui-corner-all\">";
 					}
+				elseif ($valid_form == 1){
+					echo "<input type=submit value=\" Check \" class=\"fg-button ui-state-default ui-corner-all\">";
+					}
+				}
+			else {
+				echo "<p><strong>There are no collections in the archive. Please create at least one to continue.</strong>";
+				}
 
-				echo "</form>";
+			echo "</form>";
 			?>
 
 		</div>
--- www/add_from_field.php
+++ www/add_from_field.php
@@ -121,14 +121,19 @@ setcookie("random_upload_dir", $random_dir, time()+(3600*24*30), $app_dir);
 				$temp_add_dir=query_one("SELECT Value from PumilioSettings WHERE Settings='temp_add_dir'", $connection);
 				if ($temp_add_dir == ""){
 					echo "<div class=\"error\">The local directory is not set. Please set it up in the <a href=\"admin.php\">Administration</a> menu.</div>";
+					$valid_form = 0;
 					}
-				$localdir = $temp_add_dir;
-				echo "<p>Server local directory: <input name=\"localdir\" type=\"text\" maxlength=\"160\" size=\"50\" value=\"$localdir\" class=\"fg-button ui-state-default ui-corner-all\">
-					<input type=\"hidden\" name=\"local\" value=\"1\">";
+				else{
+					$localdir = $temp_add_dir;
+					echo "<p>Server local directory: <input name=\"localdir\" type=\"text\" maxlength=\"160\" size=\"50\" value=\"$localdir\" class=\"fg-button ui-state-default ui-corner-all\">";
+					$valid_form = 1;
+					}
+					echo "<input type=\"hidden\" name=\"local\" value=\"1\">";
 				}
 			else {
 				echo"<div id=\"html5_uploader\" style=\"width: 650px; height: 330px;\">You browser doesn't support native upload. Try Firefox 3 or Safari 4.</div>
 				<br style=\"clear: both\" />";
+				$valid_form = 1;
 				}
 				
 			$query = "SELECT * from Collections ORDER BY CollectionName";
@@ -137,55 +142,62 @@ setcookie("random_upload_dir", $random_dir, time()+(3600*24*30), $app_dir);
 			$nrows = mysqli_num_rows($result);
 		
 			if ($nrows>0) {
-			echo "<p>Add files to this collection: 
+				echo "<p>Add files to this collection: 
 				<select name=\"ColID\" class=\"ui-state-default ui-corner-all\">
 					<option></option>\n";
 	
-			for ($i=0;$i<$nrows;$i++) {
-				$row = mysqli_fetch_array($result);
-				extract($row);
-					echo "<option value=\"$ColID\">$CollectionName</option>\n";
-				}
+				for ($i=0;$i<$nrows;$i++) {
+					$row = mysqli_fetch_array($result);
+					extract($row);
+						echo "<option value=\"$ColID\">$CollectionName</option>\n";
+					}
 
-			echo "</select> <a href=\"add_collection.php\">Add Collections</a><br>
+				echo "</select> <a href=\"add_collection.php\">Add Collections</a><br>
 		
-			Site: ";
-			$query_s = "SELECT SiteID AS this_SiteID, SiteName AS this_SiteName, SiteLat AS this_SiteLat, SiteLon AS this_SiteLon FROM Sites ORDER BY this_SiteName";
-			$result_s = mysqli_query($connection, $query_s)
-				or die (mysqli_error($connection));
-			$nrows_s = mysqli_num_rows($result_s);
-			echo "<select name=\"SiteID\" class=\"ui-state-default ui-corner-all\">
-				<option></option>\n";
+				Site: ";
+				$query_s = "SELECT SiteID AS this_SiteID, SiteName AS this_SiteName, SiteLat AS this_SiteLat, SiteLon AS this_SiteLon FROM Sites ORDER BY this_SiteName";
+				$result_s = mysqli_query($connection, $query_s)
+					or die (mysqli_error($connection));
+				$nrows_s = mysqli_num_rows($result_s);
+				echo "<select name=\"SiteID\" class=\"ui-state-default ui-corner-all\">
+					<option></option>\n";
 
-			for ($j=0;$j<$nrows_s;$j++) {
-				$row_s = mysqli_fetch_array($result_s);
-				extract($row_s);
-				echo "<option value=\"$this_SiteID\">$this_SiteName ($this_SiteLat/$this_SiteLon)</option>\n";
-				}
-			echo "</select>";
+				for ($j=0;$j<$nrows_s;$j++) {
+					$row_s = mysqli_fetch_array($result_s);
+					extract($row_s);
+					echo "<option value=\"$this_SiteID\">$this_SiteName ($this_SiteLat/$this_SiteLon)</option>\n";
+					}
+				echo "</select>";
 			
-			echo " <a href=\"#\" onclick=\"window.open('include/addsite.php', 'addsite', 'width=650,height=350,status=yes,resizable=yes,scrollbars=auto')\">Add sites</a><br>
+				echo " <a href=\"#\" onclick=\"window.open('include/addsite.php', 'addsite', 'width=650,height=350,status=yes,resizable=yes,scrollbars=auto')\">Add sites</a><br>
 			
-			Sensor: ";
-			$query_s = "SELECT * FROM Sensors ORDER BY SensorID";
-			$result_s = mysqli_query($connection, $query_s)
-				or die (mysqli_error($connection));
-			$nrows_s = mysqli_num_rows($result_s);
-			echo "<select name=\"SensorID\" class=\"ui-state-default ui-corner-all\">
-				<option></option>\n";
+				Sensor: ";
+				$query_s = "SELECT * FROM Sensors ORDER BY SensorID";
+				$result_s = mysqli_query($connection, $query_s)
+					or die (mysqli_error($connection));
+				$nrows_s = mysqli_num_rows($result_s);
+				echo "<select name=\"SensorID\" class=\"ui-state-default ui-corner-all\">
+					<option></option>\n";
 
-			for ($j=0;$j<$nrows_s;$j++) {
-				$row_s = mysqli_fetch_array($result_s);
-				extract($row_s);
-				echo "<option value=\"$SensorID\">$Recorder $Microphone - $Notes</option>\n";
-				}
-			echo "</select>";
+				for ($j=0;$j<$nrows_s;$j++) {
+					$row_s = mysqli_fetch_array($result_s);
+					extract($row_s);
+					echo "<option value=\"$SensorID\">$Recorder $Microphone - $Notes</option>\n";
+					}
+				echo "</select>";
 			
-			echo " <a href=\"admin.php?t=4\">Add sensors</a>
+				echo " <a href=\"admin.php?t=4\">Add sensors</a>
 			
-			<input type=\"hidden\" name=\"sm\" value=\"$sm\"><br>
-			<input type=submit value=\" Next step \" class=\"fg-button ui-state-default ui-corner-all\">
-			</form>";
+				<input type=\"hidden\" name=\"sm\" value=\"$sm\"><br>";
+			
+				if ($valid_form == 0){
+					echo "<input type=submit value=\" Next step \" DISABLED class=\"fg-button ui-state-default ui-corner-all\">";
+					}
+				elseif ($valid_form == 1){
+					echo "<input type=submit value=\" Next step \" class=\"fg-button ui-state-default ui-corner-all\">";
+					}
+			
+				echo "</form>";
 				}
 			else {
 				echo "<p><strong>There are no collections in the archive. Please create at least <a href=\"add_collection.php\">one collection</a> to continue.</strong>";
