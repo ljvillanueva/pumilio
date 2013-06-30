9d4cbdccc5af54920f927c49ba09c040f7a35780
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-06-30 19:41:14 -0400

    Rewriting the xml file, default prints sites and collections, query returns results per site or collection in query.

--- www/xml.php
+++ www/xml.php
@@ -25,10 +25,13 @@ if ($use_xml==""){
 if ($use_xml==0) {
 	}
 else{
+
+	$type = filter_var($_GET["type"], FILTER_SANITIZE_STRING);
+		
 	#XML Format
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 	<pumilio>
-	<pumilio_xml_version>1.0</pumilio_xml_version>";
+	<pumilio_xml_version>1.1</pumilio_xml_version>";
 	if ($app_admin_email==""){
 		$app_admin_email="unknown";
 		}
@@ -39,36 +42,80 @@ else{
 	<pumilio_logo>$app_url/$app_logo</pumilio_logo>
 	<pumilio_url>$app_url</pumilio_url>\n";
 
-	$query = "SELECT DISTINCT SiteID from Sounds WHERE Sounds.SoundStatus='0'";
-	$result = mysqli_query($connection, $query)
-		or die (mysqli_error($connection));
-	$nrows = mysqli_num_rows($result);
-
-	if ($nrows>0) {
-		for ($q=0;$q<$nrows;$q++) {
-			$row = mysqli_fetch_array($result);
-			extract($row);
-
-			$query1 = "SELECT * from Sites WHERE SiteID='$SiteID'";
-			$result1 = mysqli_query($connection, $query1)
-				or die (mysqli_error($connection));
-
-			$row1 = mysqli_fetch_array($result1);
-			extract($row1);
-
-			echo "<site>
-			<SiteID>$SiteID</SiteID>
-			<SiteName>$SiteName</SiteName>
-			<latitude>$SiteLat</latitude>
-			<longitude>$SiteLon</longitude>
-			<elevation>$SiteElevation</elevation>
-			<notes>$SiteNotes</notes>\n";
+	if ($type == ""){
 	
-			$query_s = "SELECT *, DATE_FORMAT(Date, '%d') AS day, DATE_FORMAT(Date, '%c') AS month, DATE_FORMAT(Date, '%Y') AS year, DATE_FORMAT(Time, '%H') AS hour, DATE_FORMAT(Time, '%i') AS minutes, DATE_FORMAT(Time, '%s') AS seconds from Sounds WHERE SiteID='$SiteID'";
-			$result_s = mysqli_query($connection, $query_s)
-				or die (mysqli_error($connection));
-			$nrows_s = mysqli_num_rows($result_s);
+		$query = "SELECT DISTINCT SiteID from Sounds WHERE Sounds.SoundStatus!='9'";
+		$result = mysqli_query($connection, $query)
+			or die (mysqli_error($connection));
+		$nrows = mysqli_num_rows($result);
 
+		if ($nrows>0) {
+			echo "<Sites>";
+			for ($q=0;$q<$nrows;$q++) {
+				$row = mysqli_fetch_array($result);
+				extract($row);
+
+				$query1 = "SELECT * from Sites WHERE SiteID='$SiteID'";
+				$result1 = mysqli_query($connection, $query1)
+					or die (mysqli_error($connection));
+
+				$row1 = mysqli_fetch_array($result1);
+				extract($row1);
+
+				echo "<site>
+				<SiteID>$SiteID</SiteID>
+				<SiteName>$SiteName</SiteName>
+				<latitude>$SiteLat</latitude>
+				<longitude>$SiteLon</longitude>
+				<elevation>$SiteElevation</elevation>
+				<notes>$SiteNotes</notes>
+				</site>\n";
+				}
+			echo "</Sites>";
+			}
+		
+		$query = "SELECT DISTINCT ColID from Sounds WHERE Sounds.SoundStatus!='9'";
+		$result = mysqli_query($connection, $query)
+			or die (mysqli_error($connection));
+		$nrows = mysqli_num_rows($result);
+
+		if ($nrows>0) {
+			echo "<Collections>";
+			for ($q=0;$q<$nrows;$q++) {
+				$row = mysqli_fetch_array($result);
+				extract($row);
+
+				$query1 = "SELECT * from Collections WHERE ColID='$ColID'";
+				$result1 = mysqli_query($connection, $query1)
+					or die (mysqli_error($connection));
+
+				$row1 = mysqli_fetch_array($result1);
+				extract($row1);
+
+				echo "<collection>
+				<ColID>$ColID</ColID>
+				<CollectionName>$CollectionName</CollectionName>
+				<Author>$Author</Author>
+				<FilesSource>$FilesSource</FilesSource>
+				<CollectionFullCitation>$CollectionFullCitation</CollectionFullCitation>
+				<URL>$MiscURL</URL>
+				<Notes>$Notes</Notes>
+				</collection>\n";
+	
+				}
+			echo "</Collections>";
+			}
+		}
+	elseif ($type == "col"){
+		$ColID = filter_var($_GET["ColID"], FILTER_SANITIZE_NUMBER_INT);
+
+		$query_s = "SELECT *, DATE_FORMAT(Date, '%d') AS day, DATE_FORMAT(Date, '%c') AS month, DATE_FORMAT(Date, '%Y') AS year, DATE_FORMAT(Time, '%H') AS hour, DATE_FORMAT(Time, '%i') AS minutes, DATE_FORMAT(Time, '%s') AS seconds from Sounds WHERE ColID='$ColID' AND SoundStatus!='9'";
+		$result_s = mysqli_query($connection, $query_s)
+			or die (mysqli_error($connection));
+		$nrows_s = mysqli_num_rows($result_s);
+		
+		if ($nrows_s > 0) {
+			echo "<Sounds>";
 			for ($s=0;$s<$nrows_s;$s++) {
 				$row_s = mysqli_fetch_array($result_s);
 				extract($row_s);
@@ -97,7 +144,48 @@ else{
 					<AudioPreviewFilePath>$app_url/sounds/previewsounds/$ColID/$DirID/$AudioPreviewFilename</AudioPreviewFilePath>
 				</sound>\n";
 				}
-			echo "</site>\n";
+			echo "</Sounds>";
+			}
+		}
+	elseif ($type == "site"){
+		$SiteID = filter_var($_GET["SiteID"], FILTER_SANITIZE_NUMBER_INT);
+		
+		$query_s = "SELECT *, DATE_FORMAT(Date, '%d') AS day, DATE_FORMAT(Date, '%c') AS month, DATE_FORMAT(Date, '%Y') AS year, DATE_FORMAT(Time, '%H') AS hour, DATE_FORMAT(Time, '%i') AS minutes, DATE_FORMAT(Time, '%s') AS seconds from Sounds WHERE SiteID='$SiteID' AND SoundStatus!='9'";
+		$result_s = mysqli_query($connection, $query_s)
+			or die (mysqli_error($connection));
+		$nrows_s = mysqli_num_rows($result_s);
+
+		if ($nrows_s > 0) {
+			echo "<Sounds>";
+			for ($s=0;$s<$nrows_s;$s++) {
+				$row_s = mysqli_fetch_array($result_s);
+				extract($row_s);
+				echo "<sound>
+					<SoundID>$SoundID</SoundID>
+					<OriginalFilename>$OriginalFilename</OriginalFilename>
+					<SoundName>$SoundName</SoundName>
+					<day>$day</day>
+					<month>$month</month>
+					<year>$year</year>
+					<hour>$hour</hour>
+					<minutes>$minutes</minutes>
+					<seconds>$seconds</seconds>
+					<SamplingRate>$SamplingRate</SamplingRate>
+					<BitRate>$BitRate</BitRate>
+					<Channels>$Channels</Channels>
+					<Duration>$Duration</Duration>
+					<SoundFormat>$SoundFormat</SoundFormat>
+					<SensorID>$SensorID</SensorID>
+					<Notes>$Notes</Notes>
+					<timestamp>$stamp</timestamp>
+					<FileSize>$FileSize</FileSize>
+					<FilePath>$app_url/sounds/sounds/$ColID/$DirID/$OriginalFilename</FilePath>
+					<AudioPreviewFormat>$AudioPreviewFormat</AudioPreviewFormat>
+					<AudioPreviewFilename>$AudioPreviewFilename</AudioPreviewFilename>
+					<AudioPreviewFilePath>$app_url/sounds/previewsounds/$ColID/$DirID/$AudioPreviewFilename</AudioPreviewFilePath>
+				</sound>\n";
+				}
+			echo "</Sounds>";
 			}
 		}
 	echo "</pumilio>";
